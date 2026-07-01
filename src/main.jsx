0a070e4228515819111c86ee2e1f1111049ee2e5
Joe <joedac3434@gmail.com>
2026-06-30 20:36:53 -0400

    Add PostHog analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
 import './index.css'
 import App from './App.jsx'
 
+posthog.init('phc_ttRQobQ8V6qoENHvehxX64SuQFCctjXfyHhnDRqak2h6',{
+  api_host: 'https://us.i.posthog.com',
+  person_profiles: 'identified_only', // cost-saver: only bills for logged-in users
+  capture_pageview: false,            // PWA has no page loads; you'll fire events manually
+  autocapture: false,                 // avoid noise/garbage from SW + slip builder clicks
+})
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <PostHogProvider client={posthog}>
+      <App />
+    </PostHogProvider>
   </StrictMode>,
 )
